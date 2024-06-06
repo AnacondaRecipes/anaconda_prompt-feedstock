ce92defaee153b916441034f372e4f2bf6474ec8
Marco Esters <mesters@anaconda.com>
2024-06-06 13:23:45 -0700

    Only copy json file for unix

--- recipe/build_unix.sh
+++ recipe/build_unix.sh
@@ -1,5 +1,5 @@
 mkdir -p "${PREFIX}/Menu"
-sed "s/__PKG_VERSION__/${PKG_VERSION}/" "${RECIPE_DIR}/menu.json" > "${PREFIX}/Menu/${PKG_NAME}_menu.json"
+cp "${RECIPE_DIR}/menu.json" "${PREFIX}/Menu/${PKG_NAME}_menu.json"
 if [[ $(uname) == Darwin ]]; then
     ROOT_PREFIX=$(${CONDA_PYTHON_EXE} -c "from conda.base.context import context; print(context.root_prefix)")
     sed -e "s/__PREFIX__/${PREFIX}/" -e "s/__ROOT_PREFIX__/${ROOT_PREFIX}/" ${RECIPE_DIR}/anaconda_prompt_osx > ${PREFIX}/anaconda_prompt
