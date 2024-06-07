e539aa13ccaf27f393773686495b855efaeacb12
Marco Esters <mesters@anaconda.com>
2024-06-07 07:46:51 -0700

    Move MacOS prompt to PREFIX/bin

--- recipe/build_unix.sh
+++ recipe/build_unix.sh
@@ -1,9 +1,10 @@
 mkdir -p "${PREFIX}/Menu"
 cp "${RECIPE_DIR}/menu.json" "${PREFIX}/Menu/${PKG_NAME}_menu.json"
 if [[ $(uname) == Darwin ]]; then
+    mkdir -p "${PREFIX}/bin"
     ROOT_PREFIX=$(${CONDA_PYTHON_EXE} -c "from conda.base.context import context; print(context.root_prefix)")
-    sed -e "s/__PREFIX__/${PREFIX}/" -e "s/__ROOT_PREFIX__/${ROOT_PREFIX}/" ${RECIPE_DIR}/anaconda_prompt_osx > ${PREFIX}/anaconda_prompt
-    chmod 554 ${PREFIX}/anaconda_prompt
+    sed -e "s/__PREFIX__/${PREFIX}/" -e "s/__ROOT_PREFIX__/${ROOT_PREFIX}/" ${RECIPE_DIR}/anaconda_prompt_osx > "${PREFIX}/bin/anaconda_prompt"
+    chmod 554 "${PREFIX}/bin/anaconda_prompt"
     ICON_EXT="icns"
 else
     ICON_EXT="png"
