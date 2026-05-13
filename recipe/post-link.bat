03d3eefc3ff957634d6a598bb9264a6b1e98758b
Robin <34315751+lrandersson@users.noreply.github.com>
2026-05-13 10:16:23 -0400

    Update check

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -13,11 +13,8 @@ IF EXIST "%CONDA_PYTHON_EXE%" (
 )
 
 IF EXIST "%CONDA_EXE%" (
-    REM Suppress the output of the command in case it returns
-    "%CONDA_EXE%" python -V >nul 2>&1
-
-    REM Note that this implies "if not errorlevel equal to or greater than 1"
-    if not errorlevel 1 (
+    REM Check if CONDA_EXE points to conda-standalone and not regular conda
+    "%CONDA_EXE%" python -V >nul 2>&1 && (
         SET PYTHON_CMD="%CONDA_EXE%" python
         GOTO :get_menuinst
     )
